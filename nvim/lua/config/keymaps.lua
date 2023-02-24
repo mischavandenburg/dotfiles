a91829895139e2dcfb8eae9d09fc6b3839fb2ce5
Mischa van den Burg <mischa@mac-beast.local>
2023-02-24 09:22:21 +0100

    added comp toggle keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set(
+  "n",
+  "<leader>uP",
+  '<cmd>lua require("cmp").setup { enabled = true }<cr>',
+  { desc = "Enable completion" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>up",
+  '<cmd>lua require("cmp").setup { enabled = false }<cr>',
+  { desc = "Disable completion" }
+)
