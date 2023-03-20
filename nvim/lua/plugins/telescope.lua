47bda552e94358a6d068552a3130a6bc7cf4c7df
Mischa van den Burg <mischa@mac-beast>
2023-03-20 09:58:09 +0100

    ajust telescope to follow symbolic links

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      pickers = {
+        find_files = {
+          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "-L" },
+        },
+      },
+    },
+  },
+}
+
+-- Custom ripgrep configuration:
+
+-- I want to search in hidden/dot files.
+-- "--hidden"
+--
+-- I don't want to search in the `.git` directory.
+-- "--glob")
+-- "!**/.git/*")
+--
+--  I want to follow symbolic links
+-- "-L"
+--
