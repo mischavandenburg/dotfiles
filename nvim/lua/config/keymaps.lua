f766113f0f16b8878ae304ab0b06e926678c21e4
Mischa van den Burg <mischavandenburg@gmail.com>
2023-04-19 13:05:03 +0200

    new keybinds h1

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -15,6 +15,8 @@ vim.keymap.set(
 
 -- insert the date in my desired configuration
 vim.keymap.set("n", "<leader>d", "<cmd>r!gendate<cr>", { desc = "Insert date" })
+vim.keymap.set("n", "<leader>h1", "<cmd>r!gendate h 1<cr>", { desc = "Insert date h1" })
+vim.keymap.set("n", "<leader>h2", "<cmd>r!gendate h 2<cr>", { desc = "Insert date h2" })
 
 -- surrounding words
 vim.keymap.set("n", "<leader>wsq", 'ciw""<Esc>P', { desc = "Word Surround Quotes" })
