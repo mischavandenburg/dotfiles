d2e52cff66a4a77058d3095e06b99c986bc56d6e
Mischa van den Burg <mischavandenburg@gmail.com>
2023-06-24 08:42:10 +0200

    add copilot

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -10,7 +10,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
+    { import = "lazyvim.plugins.extras.coding.copilot" }, -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
