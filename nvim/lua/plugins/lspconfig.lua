abbf6882ea25f0c8d017d3943485d21b939b3199
Mischa <mischavandenburg@gmail.com>
2023-10-06 04:47:03 +0200

    disable go neovim

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -6,9 +6,9 @@ return {
       ---@type lspconfig.options
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
+        -- pyright = {},
         bashls = {},
-        gopls = {},
+        -- gopls = {},
         bicep = {},
         -- yamlls = {},
         marksman = {},
