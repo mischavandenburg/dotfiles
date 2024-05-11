68296b1589746178ef4e1ad9ece75ab4fcf5d3d8
Mischa van den Burg <mischavandenburg@gmail.com>
2024-05-11 09:37:40 +0200

    vim: add pandoc support

--- nvim/lua/plugins/presenting.lua
+++ nvim/lua/plugins/presenting.lua
@@ -3,6 +3,16 @@ return {
   opts = {
     -- fill in your options here
     -- see :help Presenting.config
+    separator = {
+      -- Separators for different filetypes.
+      -- You can add your own or oberwrite existing ones.
+      -- Note: separators are lua patterns, not regexes.
+      markdown = "^#+ ",
+      pandoc = "^#+ ",
+      org = "^*+ ",
+      adoc = "^==+ ",
+      asciidoctor = "^==+ ",
+    },
   },
   cmd = { "Presenting" },
 }
