e0802188776de940b7682ea68b08ac7adb78ba63
Mischa van den Burg <mischavandenburg@gmail.com>
2024-05-21 09:58:20 +0200

    vim: add lsp again

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,23 +1,23 @@
 return {
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   ---@class PluginLspOpts
-  --   opts = {
-  --     ---@type lspconfig.options
-  --     servers = {
-  --       -- pyright will be automatically installed with mason and loaded with lspconfig
-  --       -- pyright = {},
-  --       -- bashls = {},
-  --       -- gopls = {},
-  --       -- bicep = {},
-  --       -- yamlls = {},
-  --       -- marksman = {},
-  --       -- powershell_es = {},
-  --       -- azure_pipelines_ls = {},
-  --       -- terraformls = {},
-  --       -- helm_ls = {},
-  --       -- csharp_ls = {},
-  --     },
-  --   },
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        -- pyright = {},
+        bashls = {},
+        -- gopls = {},
+        bicep = {},
+        yamlls = {},
+        marksman = {},
+        powershell_es = {},
+        -- azure_pipelines_ls = {},
+        terraformls = {},
+        helm_ls = {},
+        csharp_ls = {},
+      },
+    },
+  },
 }
